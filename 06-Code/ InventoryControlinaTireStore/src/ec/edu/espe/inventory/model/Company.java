49064b5d5fdade5e2d8e4a23c4d5b76e89e2fb38
benja1723 <84352819+benja1723@users.noreply.github.com>
2021-11-22 19:23:42 -0500

    Create class 5 and 6

---
+++ 06-Code/ InventoryControlinaTireStore/src/ec/edu/espe/inventory/model/Company.java
@@ -0,0 +1,21 @@
+
+package ec.edu.espe.inventory.model;
+
+/**
+ *
+ * @author WordBots
+ */
+public class Company {
+    
+    private String name;
+    private int telephone;
+    private String mail;
+    private String address;
+    
+    public void data(){
+        
+    }
+    public void sell(){
+        
+    }
+}
---
+++ 06-Code/ InventoryControlinaTireStore/src/ec/edu/espe/inventory/model/Price.java
@@ -0,0 +1,20 @@
+
+package ec.edu.espe.inventory.model;
+
+/**
+ *
+ * @author WordBots
+ */
+public class Price {
+    
+    private float price;
+    private int code;
+    
+    public void sell(){
+        
+    }
+    public void buy(){
+        
+    }
+    
+}
