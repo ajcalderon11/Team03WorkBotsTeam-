e58e723ad421e8a82787845eacbadb45e501fa32
ajcalderon11 <andycalde2003@gmail.com>
2021-11-22 19:46:58 -0500

    Purchase

---
+++ 06-Code/ InventoryControlinaTireStore/src/ec/edu/espe/inventory/model/Commodity.java
@@ -0,0 +1,16 @@
+
+package ec.edu.espe.inventory.model;
+
+/**
+ *
+ * @author WorkBotsTeam
+ */
+public class Commodity {
+    private String name;
+    private int code;
+    private float price;
+    
+    public void viewinstock(){
+        
+    }
+}
---
+++ 06-Code/ InventoryControlinaTireStore/src/ec/edu/espe/inventory/model/Purchase.java
@@ -0,0 +1,18 @@
+
+package ec.edu.espe.inventory.model;
+
+/**
+ *
+ * @author WorkbotsTeam
+ */
+public class Purchase {
+    private int quantity;
+    private float price;
+    private int code;
+    
+    public void time(){
+        
+    }
+    
+            
+}
