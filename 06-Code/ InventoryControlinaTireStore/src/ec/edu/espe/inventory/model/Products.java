50861cb6524a8f46d94160fa51fceccaf02e4b1e
dmcruz3 <d.mikaela2001@gmail.com>
2021-11-22 19:02:48 -0500

    Class2

---
+++ 06-Code/ InventoryControlinaTireStore/src/ec/edu/espe/inventory/model/Products.java
@@ -0,0 +1,22 @@
+
+package ec.edu.espe.inventory.model;
+
+/**
+ *
+ * @author WorBotsTeam
+ */
+public class Products {
+    private int codigo;
+    private String nombre;
+    private float precio;
+    
+    public void seeifthereisinstock(){
+    }
+    public boolean sell(){
+    return false;
+    }
+    public void buy (){
+      
+    } 
+ }
+    
