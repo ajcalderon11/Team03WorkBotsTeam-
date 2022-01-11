0b6dcc3ef774bdd774c01676eff28b8735f61aae
Seebiitaas <89808381+Seebiitaas@users.noreply.github.com>
2022-01-11 15:38:17 -0500

    FrameInventory

---
+++ 06-Code/FrameInventoryControlTireStore/FrameInventoryControlTireStore/FrmInventoryTireStore/src/ec/edu/espe/cbook/model/Connection.java
@@ -0,0 +1,32 @@
+
+package ec.edu.espe.cbook.model;
+
+
+import com.mongodb.ConnectionString;
+import com.mongodb.MongoClientSettings;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoDatabase;
+
+/**
+ *
+ * @author Caisatoa
+ */
+public class Connection {
+  //  
+    ConnectionString connectionString = new ConnectionString("mongodb+srv://dmcruz3:dmcruz3@clusteroop.lgtlx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority");
+    MongoClientSettings settings = MongoClientSettings.builder()
+        .applyConnectionString(connectionString)
+        .build();
+    //
+    MongoClient mongoClient = MongoClients.create(settings);
+    MongoDatabase database = mongoClient.getDatabase("Proyect");
+    
+    //
+    public MongoDatabase obtenerDB(){
+        return database;
+    }
+//
+}
+
+//conections
